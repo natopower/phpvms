e68d2bbecd6e713f6c6467e28485922367fcb676
natopower <natopower@gmail.com>
2021-08-21 01:22:30 -0300

    initial commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,4 +9,8 @@ include_once __DIR__.'/application.php';
 $app = new Application();
 $app->bindInterfaces();
 
+if ((env('APP_DEBUG'))) {
+    $app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
+   }
+
 return $app;
