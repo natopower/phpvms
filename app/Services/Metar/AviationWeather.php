b42135657b893f6a6be149b03d27a7ad306b2549
natopower <natopower@gmail.com>
2024-02-21 21:18:26 -0700

    maybe fixed the weather

--- app/Services/Metar/AviationWeather.php
+++ app/Services/Metar/AviationWeather.php
@@ -4,7 +4,6 @@ namespace App\Services\Metar;
 
 use App\Contracts\Metar;
 use App\Support\HttpClient;
-use function count;
 use Exception;
 use Illuminate\Support\Facades\Log;
 
@@ -13,23 +12,16 @@ use Illuminate\Support\Facades\Log;
  */
 class AviationWeather extends Metar
 {
-    private const METAR_URL =
-        'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true&stationString=';
+    private const METAR_URL = 'https://aviationweather.gov/api/data/metar?ids=';
 
-    private const TAF_URL =
-        'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true&stationString=';
-
-    /**
-     * @var HttpClient
-     */
-    private HttpClient $httpClient;
+    private const TAF_URL = 'https://aviationweather.gov/api/data/taf?ids=';
 
     /**
      * @param HttpClient $httpClient
      */
-    public function __construct(HttpClient $httpClient)
-    {
-        $this->httpClient = $httpClient;
+    public function __construct(
+        private readonly HttpClient $httpClient
+    ) {
     }
 
     /**
@@ -51,33 +43,8 @@ class AviationWeather extends Metar
         $url = static::METAR_URL.$icao;
 
         try {
-            $res = $this->httpClient->get($url, []);
-            $xml = simplexml_load_string($res);
-
-            if ($xml->errors && count($xml->errors->children()) > 0) {
-                return '';
-            }
-
-            $attrs = $xml->data->attributes();
-            if (!isset($attrs['num_results'])) {
-                return '';
-            }
-
-            $num_results = $attrs['num_results'];
-            if (empty($num_results)) {
-                return '';
-            }
-
-            $num_results = (int) $num_results;
-            if ($num_results === 0) {
-                return '';
-            }
-
-            if (count($xml->data->METAR->raw_text) === 0) {
-                return '';
-            }
-
-            return $xml->data->METAR->raw_text->__toString();
+            $raw_metar = $this->httpClient->get($url);
+            return trim($raw_metar);
         } catch (Exception $e) {
             Log::error('Error reading METAR: '.$e->getMessage());
             return '';
@@ -99,32 +66,12 @@ class AviationWeather extends Metar
             return '';
         }
 
-        $tafurl = static::TAF_URL.$icao;
+        $url = static::TAF_URL.$icao;
 
         try {
-            $tafres = $this->httpClient->get($tafurl, []);
-            $tafxml = simplexml_load_string($tafres);
-
-            $tafattrs = $tafxml->data->attributes();
-            if (!isset($tafattrs['num_results'])) {
-                return '';
-            }
-
-            $tafnum_results = $tafattrs['num_results'];
-            if (empty($tafnum_results)) {
-                return '';
-            }
-
-            $tafnum_results = (int) $tafnum_results;
-            if ($tafnum_results === 0) {
-                return '';
-            }
-
-            if (count($tafxml->data->TAF->raw_text) === 0) {
-                return '';
-            }
-
-            return $tafxml->data->TAF->raw_text->__toString();
+            $raw_taf = $this->httpClient->get($url);
+            // Remove " \n" to remove new lines from the metar content
+            return trim(str_replace(" \n", '', $raw_taf));
         } catch (Exception $e) {
             Log::error('Error reading TAF: '.$e->getMessage());
             return '';
